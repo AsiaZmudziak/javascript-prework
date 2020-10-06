b7129584bd8c4c46a9a5705fb2f832bfec5c814b
AsiaZmudziak <asiazmudziak@gmail.com>
2020-10-06 03:56:32 +0100

    add player move

--- js/script.js
+++ js/script.js
@@ -13,5 +13,21 @@ if (randomNumber == 2) {
 if (randomNumber == 3) {
     computerMove = 'nożyce';
 }
-
 printMessage('Mój ruch to: ' + computerMove);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == '1') {
+    playerMove = 'kamień';
+}
+if (playerInput == '2') {
+    playerMove = 'papier';
+}
+if (playerInput == '3') {
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
